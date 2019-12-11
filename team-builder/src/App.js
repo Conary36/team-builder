296e497df8c9f1823fb1a4096b7094867bab480b
Conary36 <cbeckford144@gmail.com>
2019-12-10 22:16:17 -0500

    updated files

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,44 +1,16 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Form from './components/Form';
+
+function App(){
 
 
-function Team(){
-  const [member, setTeamMember] = useState({ name: "", position: "" })
 
-
-  const roster = event => {
-    setTeamMember({ ...member, [event.target.name]: event.target.value });
-
-  };
-
-  return (
-
-    <div className="App">
-      {console.log(member)}
-      <form>
-        <label>
-          Name:
-          <input
-            type='text'
-            name="name"
-            onChange={event => roster(event)}
-            />
-        </label>
-
-        <label>
-            Position:
-            <input
-              type='text'
-              name='position'
-              onChange={event => roster(event)}
-              />
-        </label>
-      </form>
+  return(
+    <div>
+      <Form/>
     </div>
-
   )
-  
 }
 
 export default App;
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+// import logo from './logo.svg';
+import '../App.css';
+
+
+function Form() {
+    const [member, setTeamMember] = useState({ name: "", position: "" });
+
+
+    const roster = event => {
+        setTeamMember({ ...member, [event.target.name]: event.target.value });
+
+    };
+
+    return (
+
+        <div className="App-header">
+            {console.log(member)}
+            <form>
+                <label>
+                    Name:
+                     <input
+                        type='text'
+                        name="name"
+                        onChange={event => roster(event)}
+                        />
+                </label>
+
+                <label>
+                    Position:
+                     <input
+                        type='text'
+                        name='position'
+                        onChange={event => roster(event)}
+                        />
+                </label>
+            </form>
+        </div>
+
+    )
+
+}
+
+export default Form;
